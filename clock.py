0261d81cad74ddb72c39dd32c052f49d7c841ca2
Waltteri Rintala <rintala.waltteri@gmail.com>
2021-01-23 11:56:03 +0200

    added scheduler to prevent server sleeping

--- clock.py
+++ clock.py
@@ -1,12 +1,19 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from app import Hour
+from datetime import datetime
 from datafetchers.fetch_all import fetch_all
+import requests
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', hours=1)
+@sched.scheduled_job('interval', hours=1, next_run_time=datetime.now())
 def scrape_info_to_db():
-    print("SAVING")
+    #print("SAVING")
     Hour(data=fetch_all()).save()
+
+@sched.scheduled_job('interval', minutes=20)
+def prevent_free_tier_sleeping():
+    requests.get("http://top5uutiset.herokuapp.com/api/metadata")
+
 
 sched.start()
